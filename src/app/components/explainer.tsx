f66c570f6f89cb6c009f4f74609e2ad3b488912f
bhavik singh <bhavik@mac.lan>
2025-06-05 20:36:39 -0400

    publish the website please

--- src/app/components/explainer.tsx
+++ src/app/components/explainer.tsx
@@ -9,8 +9,8 @@ const Explainer = () => {
     const { incrementStage, setStage } = useModalStage();
     return (
         <div className={modalCSS}>
-            <div className="h4">welcome to june7.site</div>
-            <p>my name is bhavik, and i love building websites with people. today is june 7, 2025 & i'm building a website from scratch — with you and many (maybe hundreds) of other people.</p>
+            <div className="h2">help me make this website today!</div>
+            <p>my name is bhavik, and i love building websites with people. today i'm building this website from scratch — with you and many (maybe hundreds?) of other people.</p>
             <p>help shape what this website will become by submitting a request below. i will try my best to cater to the needs of every single request submitted.</p>
             <div className={buttonCSS} onClick={() => incrementStage(1)}>make a request</div>
             <div className="stack:horizontal fullWidth"><div className={buttonCSSSecondary + " fullWidth"} onClick={() => setStage(2)}>see requests</div><div className={buttonCSSSecondary + " fullWidth"} onClick={close}>explore site</div></div>
--- src/app/components/guide.tsx
+++ src/app/components/guide.tsx
@@ -31,5 +31,5 @@ const GuideOpener = () => {
     const { open } = useModalIsOpen();
     const { requestCount } = useRequests();
     const { setStage } = useModalStage();
-    return <div className="padded:s-2 fullWidth position:fixed fill:contrast color:white center:text"><span className="underline cursor:pointer" onClick={() => { setStage(0); open() }}>make a request to shape this website</span></div>
+    return <div className="padded:s-2 fullWidth position:fixed fill:contrast color:white center:text"><span className="underline cursor:pointer h2" onClick={() => { setStage(0); open() }}>help me make this website!</span></div>
 }
