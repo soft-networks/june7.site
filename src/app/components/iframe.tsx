986bcd1a4f2d4cf87cdfeb875b201ad2f7d50c04
bhavik singh <bhavik@bhaviks-WorkBook-2.local>
2025-06-07 12:49:13 -0400

    fixing iframe issue

--- src/app/components/iframe.tsx
+++ src/app/components/iframe.tsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from 'react';
 export default function IFrame() {
 
     return (
-        <div className="fullBleed noOverflow zIndex:behind">
+        <div className="fullScreen noOverflow zIndex:behind">
             <iframe 
                 src="https://june7internal.netlify.app" 
-                className="noBorder fullBleed" 
+                className="noBorder fullScreen" 
             />
         </div>
     )
